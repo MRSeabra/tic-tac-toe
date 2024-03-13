2c0ce4531826061af1813c5459d1df42a4950ceb
MRSeabra <maraceliarodrigues9@gmail.com>
2024-03-13 17:14:26 -0300

    localstorage/box-shadow

--- js/mainjs.js
+++ js/mainjs.js
@@ -1,13 +1,15 @@
 // usuario escolhe se quer O ou X
 const btns = document.querySelectorAll(".btn-choice");
+let btnKey = "btnChoice";
+localStorage.setItem(btnKey, "btnX");
 
 function toggleClass(item) {
   btns.forEach(function (btn) {
     btn.classList.remove("active");
-    localStorage.removeItem("id");
+    localStorage.removeItem(btnKey, "");
   });
   item.currentTarget.classList.add("active");
-  localStorage.setItem("id", "myvalue");
+  localStorage.setItem(btnKey, item.currentTarget.id);
 }
 
 btns.forEach(function (btn) {
